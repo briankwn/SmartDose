a0e9519015e8f7b867d5a8c2a12aadc3d6f1cd42
SUVEER GARG <gsuveer@park.key.seas.upenn.edu>
2019-11-12 16:33:03 -0500

    s

--- Firmware/WINC1500_HTTP_DOWNLOADER_EXAMPLE1/src/main21.c
+++ Firmware/WINC1500_HTTP_DOWNLOADER_EXAMPLE1/src/main21.c
@@ -904,7 +904,13 @@ void extint_detection_callback(void)
 	
 }
 
+/**
 
+* function      otafu()
+* @brief        Downloads hosted files (MAIN_HTTP_FILE_URL and PARAMS_HTTP_FILE_URL) 
+* @param[in]    void
+* @return       void : We reset so that the new file can be written through bootloader
+**/
 static void otafu(void){
 
 	do_download_flag = true;
@@ -948,19 +954,96 @@ static void otafu(void){
 		sw_timer_task(&swt_module_inst);
 	}
 	printf("otafu: done.\r\n");
-	system_reset();
-	socketDeinit();
-	delay_s(1);
-	configure_mqtt();
-	socketInit();
-	registerSocketCallback(socket_event_handler, socket_resolve_handler);
-	if (mqtt_connect(&mqtt_inst, main_mqtt_broker))
-	{
-		printf("Error connecting to MQTT Broker!\r\n");
-	}
-	printf("otafu: done.\r\n");
+	uint8_t result_crc= check_crc();	
 	
+	system_reset();
 }
+
+/**
+
+* function      check_crc()
+* @brief        Check CRC between downloaded file and one obtained from server
+* @param[in]    void
+* @return       uint8_t : 0 if failed 1 if successful 
+
+**/
+uint8_t check_crc()
+{
+	char firmware_file_name[] = "0:app.bin";
+	FIL firmware_file;
+	FRESULT res;
+	uint32_t page_size = 64;		//Number of bytes per page --//page size is 64 bytes
+	uint32_t total_pages = (firmware_file.fsize / page_size);
+	SerialConsoleWriteString("Checking \r\n");
+	char block[page_size];
+	uint32_t br;
+	crc32_t crc_calculated=0;
+	
+	res = f_open(&firmware_file, (char const *)firmware_file_name, FA_READ);
+	if (res != FR_OK) {
+		LogMessage(LOG_INFO_LVL ,"[FAIL: Could not open Firmware File] res %d\r\n", res);
+		//set result to -1, file not read correctly
+	}
+	
+	for (uint32_t i = 0 ; i < (total_pages) ; i++)
+	{
+						
+		res = f_read (&firmware_file,block, page_size, &br);
+		if (res != FR_OK) {
+				LogMessage(LOG_INFO_LVL ,"[FAIL: Could not read Block from Firmware File] res %d, bytes read %d\r\n", res, br);
+				break;
+		}
+		res = crc32_recalculate(block,page_size,&crc_calculated);
+	}
+	
+	
+	// Calculate remainder
+	uint32_t remainder = (firmware_file.fsize % page_size);
+	res = f_read (&firmware_file,block, page_size, &br);
+	if (res != FR_OK) {
+		LogMessage(LOG_INFO_LVL ,"[FAIL: Could not read Block from Firmware File] res %d, bytes read %d\r\n", res, br);
+		
+	}
+
+	res = crc32_recalculate(block,remainder,&crc_calculated);
+	
+	char params_file_name[] = "params.csv";
+	FIL params_file;
+	
+	SerialConsoleWriteString("Opening params.csv\r\n");
+	res = f_open(&params_file, (char const *)params_file_name, FA_READ);
+	if (res != FR_OK) {
+		LogMessage(LOG_INFO_LVL ,"[FAIL: Could not open params file] res %d\r\n", res);
+	}
+			
+	char params[50];
+	
+	res = f_read (&params_file,(char const *)params, 50, &br);
+	if (res != FR_OK) {
+			LogMessage(LOG_INFO_LVL ,"[FAIL: Could not read Parameters File] res %d\r\n", res);
+			
+	}
+	LogMessage(LOG_INFO_LVL ,"[Bytes read from params.csv: ] %d\r\n", br);
+			
+	f_close(&params_file);
+
+	char * flag = strtok(params, ",");
+	char * string_crc_from_file = strtok(NULL, ",");
+	char * useless;
+	uint32_t crc_from_file = strtoul(string_crc_from_file,&useless,16);
+	
+	if(crc_calculated == crc_from_file)
+	{
+		LogMessage(LOG_INFO_LVL ,"[CRC Matches]\r\n");
+		return 1;
+	}
+	else
+	{	
+		LogMessage(LOG_INFO_LVL ,"[-10 for Griffindor]\r\n");
+		return 0;
+	}
+}
+
 
 /**
  * \brief Main application function.
